bead48bb0273428a6747c37f596b284783bc4690
Melanie <1958Melanie.Castillo@gmail.com>
2025-12-09 23:08:54 -0800

    commit

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,23 +1,23 @@
+// Next.js `Head` lets you modify the document <head> for the page
+// (useful for setting the title, meta tags, etc.).
+import Head from 'next/head';
+
 // Layout provides consistent page structure (header/footer and common meta)
 // used to wrap pages so they share the same site chrome.
-import Layout from '../../components/layout';
+import Layout from '../components/layout';
 
 // Helpers for static generation: `getAllPostIds()` is used by
 // `getStaticPaths` to list all post IDs, and `getPostData(id)` fetches and
 // parses a single post's content/metadata (used in `getStaticProps`).
-import { getAllIds, getData } from '../../lib/data';
+import { getAllIds, getData } from '../lib/data';
 
-// Next.js `Head` lets you modify the document <head> for the page
-// (useful for setting the title, meta tags, etc.).
-import Head from 'next/head';
- 
 export async function getStaticProps({ params }) {
   const itemData = await getData(params.id);
- // console.log
+  // console.log(itemData);
   return {
     props: {
-      itemData,
-    },
+      itemData
+    }
   };
 }
 
@@ -38,9 +38,9 @@ export default function Entry({ itemData }) {
     <Layout>
       <article className="card col-6">
         <div className="card-body">
-         <h5 className="card-title">{itemData.post_title}</h5>
-         <h6 className="card-subtitle mb-2 text-muted">{itemData.user_login}</h6>
-         <div className="card-text" dangerouslySetInnerHTML={{__html: itemData.post_content}} />
+          <h5 className="card-title">{itemData.post_title}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">{itemData.user_login}</h6>
+          <div className="card-text" dangerouslySetInnerHTML={{__html: itemData.post_content}} />
         </div>
       </article>
     </Layout>
